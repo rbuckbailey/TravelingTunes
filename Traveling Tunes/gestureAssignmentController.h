37396353da66061ff8f1d2da388adce96026a8ff
buck <buck@buckitron.local>
2014-03-11 09:49:54 -0400

    Added properties for 2, 3 finger gestures. Still need Views for this.

--- Traveling Tunes/gestureAssignmentController.h
+++ Traveling Tunes/gestureAssignmentController.h
@@ -43,6 +43,22 @@
 @property (atomic) int swipeRight;
 @property (atomic) int swipeUp;
 @property (atomic) int swipeDown;
+@property (atomic) int singleTap2;
+@property (atomic) int doubleTap2;
+@property (atomic) int longPress2;
+@property (atomic) int tripleTap2;
+@property (atomic) int swipeLeft2;
+@property (atomic) int swipeRight2;
+@property (atomic) int swipeUp2;
+@property (atomic) int swipeDown2;
+@property (atomic) int singleTap3;
+@property (atomic) int doubleTap3;
+@property (atomic) int longPress3;
+@property (atomic) int tripleTap3;
+@property (atomic) int swipeLeft3;
+@property (atomic) int swipeRight3;
+@property (atomic) int swipeUp3;
+@property (atomic) int swipeDown3;
 
 @end
 
--- Traveling Tunes/gestureAssignmentController.m
+++ Traveling Tunes/gestureAssignmentController.m
@@ -21,6 +21,22 @@
     _swipeRight = NEXTSONG;
     _swipeUp    = VOLUMEUP;
     _swipeDown  = VOLUMEDOWN;
+    _singleTap2  = UNASSIGNED;
+    _longPress2  = UNASSIGNED;
+    _doubleTap2  = UNASSIGNED;
+    _tripleTap2  = UNASSIGNED;
+    _swipeLeft2  = UNASSIGNED;
+    _swipeRight2 = UNASSIGNED;
+    _swipeUp2    = UNASSIGNED;
+    _swipeDown2  = UNASSIGNED;
+    _singleTap3  = UNASSIGNED;
+    _longPress3  = UNASSIGNED;
+    _doubleTap3  = UNASSIGNED;
+    _tripleTap3  = UNASSIGNED;
+    _swipeLeft3  = UNASSIGNED;
+    _swipeRight3 = UNASSIGNED;
+    _swipeUp3    = UNASSIGNED;
+    _swipeDown3  = UNASSIGNED;
     return self;
 }
 @end
