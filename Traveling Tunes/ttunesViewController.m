d584e194c75ec76cbd363352c47bec78dac884c4
Buck Bailey <r.buck.bailey@gmail.com>
2014-03-23 20:48:50 -0400

    Song titles and default text set up. Still need scaling / marquee. And colors. And song picker.

--- Traveling Tunes/ttunesViewController.m
+++ Traveling Tunes/ttunesViewController.m
@@ -34,6 +34,8 @@ MPMusicPlayerController*        mediaPlayer;
     [self setupLabels];
 }
 
+/*** Gesture Actions begin ************************************************************************************************************************/
+
 - (IBAction)twoFingerTap:(id)sender {
     _songTitle.text=@"Two Fingers Detected";
 }
@@ -137,19 +139,19 @@ MPMusicPlayerController*        mediaPlayer;
     [self performPlayerAction:[assignments objectForKey:@"1SwipeDown"]:@"swipeDown"];
 }
 
+/****** Gesture Actions end *********************************************************************************************************************************/
+/****** Player Actions begin *********************************************************************************************************************************/
 
 - (void)performPlayerAction:(NSString *)action :(NSString*)sender {
-//    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"mainStoryboard" bundle:nil];
-//    settingsTableViewController *viewController = (settingsTableViewController *) [storyboard instantiateViewControllerWithIdentifier:@"settingsTable"];
-    if ([action isEqual:@"Unassigned"]) _songTitle.text = [NSString stringWithFormat:@"%@ sent unassigned command",sender];
+    if ([action isEqual:@"Unassigned"]) NSLog(@"%@ sent unassigned command",sender);
     else if ([action isEqual:@"Menu"]) [self performSegueWithIdentifier: @"goToSettings" sender: self];
     else if ([action isEqual:@"PlayPause"]) _songTitle.text = @"playPause";
     else if ([action isEqual:@"Play"]) _songTitle.text = @"play";
     else if ([action isEqual:@"Pause"]) _songTitle.text = @"Pause";
-    else if ([action isEqual:@"NextSong"]) { _songTitle.text = @"skipToNext"; [mediaPlayer skipToNextItem]; }
-    else if ([action isEqual:@"PreviousSong"]) { _songTitle.text = @"prevOrRestart"; [mediaPlayer skipToPreviousItem]; }
-    else if ([action isEqual:@"RestartPrevious"]) { _songTitle.text = @"prevOrRestart"; [mediaPlayer skipToPreviousItem]; }
-    else if ([action isEqual:@"Restart"]) { _songTitle.text = @"Restart"; [mediaPlayer skipToPreviousItem]; }
+    else if ([action isEqual:@"NextSong"]) { [mediaPlayer skipToNextItem]; }
+    else if ([action isEqual:@"PreviousSong"]) { [mediaPlayer skipToPreviousItem]; }
+    else if ([action isEqual:@"RestartPrevious"]) { [mediaPlayer skipToPreviousItem]; }
+    else if ([action isEqual:@"Restart"]) { [mediaPlayer skipToPreviousItem]; }
     else if ([action isEqual:@"Rewind"]) _songTitle.text = @"rewind";
     else if ([action isEqual:@"FastForward"]) _songTitle.text = @"FF";
     else if ([action isEqual:@"VolumeUp"]) _songTitle.text = @"vol up";
@@ -197,91 +199,14 @@ MPMusicPlayerController*        mediaPlayer;
     NSLog(@"Size found with width %f and height %f", width, height);
 }
 */
-- (void)fitLongTitle:(NSString *)input {
-    NSString *text = input;
-    CGFloat width = _artistTitle.frame.size.width;
-    CGFloat height = _artistTitle.frame.size.height;
-    bool sizeFound = false;
-    float size = 50;
-    while (!sizeFound) {
-        NSLog(@"Begin loop");
-        CGFloat fontSize = 14;
-        CGFloat previousSize = 0.0;
-        CGFloat currSize = 0.0;
-        for (float fSize = fontSize; fSize < fontSize+12; fSize++) {
-            CGRect r = [text boundingRectWithSize:CGSizeMake(width, height)
-                                          options:NSStringDrawingUsesLineFragmentOrigin
-                                       attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:fSize]}
-                                          context:nil];
-            currSize =r.size.width*r.size.height;
-            if (previousSize >= currSize) {
-                width = width*11/10;
-                height = height*11/10;
-                fSize = fontSize+10;
-            }
-            else {
-                previousSize = currSize;
-            }
-
-            NSLog(@"fontSize = %f\tbounds = (%f x %f) = %f",
-                  fSize,
-                  r.size.width,
-                  r.size.height,r.size.width*r.size.height);
-        }
-        if (previousSize == currSize) {
-            sizeFound = true;
-        }
-        
-    }
-    _artistTitle.numberOfLines = 1;
-    _artistTitle.text   = input;
-    _artistTitle.font   = [UIFont systemFontOfSize:size];
-    _artistTitle.frame = CGRectMake(_artistTitle.frame.origin.x, _artistTitle.frame.origin.y, width, height);
-    NSLog(@"Size %f found with width %f and height %f", size, width, height);
-}
-
--(void)resizeSongTitle {
-/*    CGRect screenBound = [[UIScreen mainScreen] bounds];
-    CGSize screenSize = screenBound.size;
-    CGFloat screenWidth = screenSize.width;
-    CGFloat screenHeight = screenSize.height;
- */
-    
-}
-
-- (void)drawLabelFromScratch {
- /*   CGRect screenBound = [[UIScreen mainScreen] bounds];
-    CGSize screenSize = screenBound.size;
-    CGFloat screenWidth = screenSize.width;
-    CGFloat screenHeight = screenSize.height;
-*/
-    
-    CGRect labelFrame = CGRectMake(22, 50, _artistTitle.frame.size.width, _artistTitle.frame.size.height);
-    UILabel *myLabel = [[UILabel alloc] initWithFrame:labelFrame];
-    [myLabel setText:@"Artist Test This is a long string where does it cut off"];
-    [myLabel setBackgroundColor: [UIColor lightGrayColor]];
-    [myLabel setNumberOfLines:0];
-    
-    CGFloat fontSize = 0.0f;
-    labelFrame.size = [myLabel.text sizeWithFont:myLabel.font
-                                     minFontSize:myLabel.minimumFontSize
-                                  actualFontSize:&fontSize
-                                        forWidth:_artistTitle.frame.size.width //labelframe width from above
-                                   lineBreakMode:myLabel.lineBreakMode];
-    
-    myLabel.frame = labelFrame;
-    [self.view addSubview:myLabel];
-}
 
 /* from StackOverflow, marquee code from OSX. 
- 
+ ********************************************
 -(void)awakeFromNib{
     self.myTimer=[NSTimer new];
     self.fullString=@"This is a long string to be shown.";
     [self.label setAlignment:NSRightTextAlignment];
 }
-
-
 -(void)scrollText:(id)parameter{
     static NSInteger len;
 //    [self.label setStringValue:[self.fullString substringWithRange:NSMakeRange(0, len++)]];
@@ -292,39 +217,48 @@ MPMusicPlayerController*        mediaPlayer;
         [self.myTimer invalidate];
     }
 }
-
 - (IBAction)buttonAction:(id)sender {
-    
     self.myTimer = [NSTimer scheduledTimerWithTimeInterval:0.2f
                                                     target: self
                                                   selector:@selector(scrollText:)
                                                   userInfo: nil
                                                    repeats:YES];
 }
-*/
+************************************************/
 
 - (void)setupLabels {
     gestureAssignmentController *gestureController = [[gestureAssignmentController alloc] init];
    
-    _artistTitle.numberOfLines = 1;
-    _artistTitle.text   = @"Artist Test This is a long string where does it cut off";
+    if ([mediaPlayer.nowPlayingItem valueForProperty:MPMediaItemPropertyTitle]==NULL) { //output "none playing screen"
+        _artistTitle.numberOfLines = 1;
+        _artistTitle.text   = @"No music playing.";
+        _artistTitle.font   = [UIFont systemFontOfSize:30];
+        
+        _songTitle.numberOfLines = 1;
+        _songTitle.text   = @"Tap screen to play default playlist.";
+        _songTitle.font   = [UIFont systemFontOfSize:30];
+        
+        _albumTitle.numberOfLines = 1;
+        _albumTitle.text    = @"...";
+        _albumTitle.font    =  [UIFont systemFontOfSize:30];
+    } else { // output song titles and info
+        _artistTitle.numberOfLines = 1;
+        _artistTitle.text   = [mediaPlayer.nowPlayingItem valueForProperty:MPMediaItemPropertyArtist];
         _artistTitle.font   = [UIFont systemFontOfSize:(int)[[[gestureController displaySettings] objectForKey:@"artistFontSize"] floatValue]];
-    //  _artistTitle.frame = CGRectMake(_artistTitle.frame.origin.x, _artistTitle.frame.origin.y, _artistTitle.frame.size.width, _artistTitle.frame.size.height);
     
+        _songTitle.numberOfLines = 1;
+        _songTitle.text   = [mediaPlayer.nowPlayingItem valueForProperty:MPMediaItemPropertyTitle];
+        _songTitle.font   = [UIFont systemFontOfSize:(int)[[[gestureController displaySettings] objectForKey:@"songFontSize"] floatValue]];
+        
+        _albumTitle.numberOfLines = 1;
+        _albumTitle.font    = [UIFont systemFontOfSize:(int)[[[gestureController displaySettings] objectForKey:@"albumFontSize"] floatValue]];
+        _albumTitle.text    = [mediaPlayer.nowPlayingItem valueForProperty:MPMediaItemPropertyAlbumTitle];
 
-    _songTitle.numberOfLines = 1;
-    _songTitle.text   = @"Song Test This is a long string where does it cut off";
-    _songTitle.font   = [UIFont systemFontOfSize:(int)[[[gestureController displaySettings] objectForKey:@"songFontSize"] floatValue]];
- //   _songTitle.frame = CGRectMake(_songTitle.frame.origin.x, _songTitle.frame.origin.y-(_songTitle.frame.size.height/2), _songTitle.frame.size.width, _songTitle.frame.size.height*2);
-
-    _albumTitle.numberOfLines = 1;
-    _albumTitle.font   = [UIFont systemFontOfSize:(int)[[[gestureController displaySettings] objectForKey:@"albumFontSize"] floatValue]];
-    _albumTitle.text    = @"Album Test This is a long string where does it cut off";
-//    _albumTitle.frame = CGRectMake(_albumTitle.frame.origin.x, _albumTitle.frame.origin.y, _albumTitle.frame.size.width, _albumTitle.frame.size.height);
+    }
 }
 
 - (void)beatlesParty {
-    _songTitle.text = @"BEATLES PARTY";         //Create a query that will return all songs by The Beatles grouped by album
+    //Create a query that will return all songs by The Beatles grouped by album
     MPMediaQuery* query = [MPMediaQuery songsQuery];
     [query addFilterPredicate:[MPMediaPropertyPredicate predicateWithValue:@"The Beatles" forProperty:MPMediaItemPropertyArtist comparisonType:MPMediaPredicateComparisonEqualTo]];
     [query setGroupingType:MPMediaGroupingAlbum];
