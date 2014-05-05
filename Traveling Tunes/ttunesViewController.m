4a8854d0367f47e3c10eb918b673bcf1e9e83000
Buck Bailey <r.buck.bailey@gmail.com>
2014-05-05 10:51:27 -0400

    retuned speaking distances

--- Traveling Tunes/ttunesViewController.m
+++ Traveling Tunes/ttunesViewController.m
@@ -2157,13 +2157,10 @@ MKRoute *routeDetails;
 }
 
 -(void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance {
-    if (_playbackPausedByGPS) [self playOrDefault];
+    if (_playbackPausedByGPS) [mediaPlayer play]; //[self playOrDefault];
 }
 
 - (void)say:(NSString*)instructions {
-//    AVSpeechUtterance *utterance = [AVSpeechUtterance
-//                                    speechUtteranceWithString:instructions];
-    //strip commas to prevent awkward pausing after "at the end of the road"
     AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:instructions]; //[instructions stringByReplacingOccurrencesOfString:@"," withString:@""]];
 
     utterance.rate = 0.25;
@@ -2238,22 +2235,22 @@ MKRoute *routeDetails;
                 _firstStep = NO;
             }
             // convert distance in meters to feet before comparison; if <100 feet, announce turn
-            else if ((andThenStep.distance/3.28084)<40+_speedTier) {
+            else if ((andThenStep.distance/3.28084)<60+_speedTier) {
 //                sayWhat = [sayWhat stringByAppendingString:nextStep.instructions];
                 // only say instructions once between 100 ft,
-                if ((![_latestInstructions isEqual:andThenStep.instructions])&((andThenStep.distance/3.28084)>(10+_speedTier))) {
+                if ((![_latestInstructions isEqual:andThenStep.instructions])&((andThenStep.distance/3.28084)>(20+_speedTier))) {
                     if ([[defaults objectForKey:@"nearingTurnNoise"] isEqual:@"1"]) [self dingForUpcomingDirections]; else if ([[defaults objectForKey:@"nearingTurnNoise"] isEqual:@"0"]) [self say:sayWhat];
                     _latestInstructions = andThenStep.instructions;
                     _didSayTurn = NO;
                 }
                 // then say again at <15ft, also only once
-                if (((andThenStep.distance/3.28084)<(10+_speedTier))&!(_didSayTurn)) {
+                if (((andThenStep.distance/3.28084)<(20+_speedTier))&!(_didSayTurn)) {
                     if ([[defaults objectForKey:@"atTurnNoise"] isEqual:@"1"]) [self dingForUpcomingDirections]; else if ([[defaults objectForKey:@"atTurnNoise"] isEqual:@"0"]) [self say:sayWhat];                    _latestInstructions = andThenStep.instructions;
                     _didSayTurn = YES;
                     if (_onLastStep) _finishedNavigating = YES;
                 }
 //                _gpsDebugLabel.text = [NSString stringWithFormat:@"%@\n%@\n%@",nextStep.instructions,andThenStep.instructions,step3Step.instructions];
-                if (_finishedNavigating) [self cancelNavigation];
+//                if (_finishedNavigating) [self cancelNavigation];
                 // last, if under 100', setFireDate of the timer to 10 seconds instead of 30
                 NSDate *currentTime = [NSDate date];
                 [_GPSTimer setFireDate:[currentTime dateByAddingTimeInterval:5.0]];
@@ -2281,6 +2278,7 @@ MKRoute *routeDetails;
             [self fixGPSLabels];
         }
     }];
+    if (_finishedNavigating) [self cancelNavigation];
 }
 
 - (NSString*) symbolForDirections:(NSString*)directions {
